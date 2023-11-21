498709f9b1f973d56ddfe5b455fcc1d6010bd2fc
maria_edurada <mariaduda.mendonca.m@gmail.com>
2023-11-21 17:50:50 -0300

    adicionando html e js

---
+++ js/fufu.js
@@ -0,0 +1,23 @@
+// console.log("to guardando meu maior plano maligno");
+
+// var meu_plano_maligno = "dominar o mundo";
+// console.log(meu_plano_maligno);
+
+// var numero10 = 10;
+// var numero20 = 20;
+// var soma = numero10 + numero20
+// console.log(soma);
+
+// console.log("some daqui meu " + soma);
+// console.log(soma + " some daqui meu ");
+
+// var idade = prompt("qual sua idade? ");
+
+// if (idade > 18) {
+//     window.alert("cacudo!");
+// } else {
+//     window.alert("feto!");
+// }
+
+var nome = prompt("qual teu nome? ");
+document.getElementById("nome").innerHTML = nome;
